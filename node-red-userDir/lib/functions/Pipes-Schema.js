288038113f230fe7de939a1ab59d97d179366f71
david mallon <mallond@gmail.com>
2017-09-07 13:30:50 -0500

    add pipes

---
+++ node-red-userDir/lib/functions/Pipes-Schema.js
@@ -0,0 +1,21 @@
+// name: Pipes Schema
+{
+  "properties": {
+    "amount": {
+    "type": "number"
+    },  
+    "tax": {
+      "type": "number"
+    },
+    "surcharge": {
+    "type": "number"
+    },
+    "shipping": {
+    "type": "number"
+    },
+    "total": {
+    "type": "number"
+    }
+  },
+  "required":["amount","tax","surcharge","shipping"]
+}
